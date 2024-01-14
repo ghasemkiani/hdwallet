841ca3cb5f6b9877f99faadbfa3b31756624c17a
Ghasem Kiani <ghasemkiani@gmail.com>
2024-01-15 00:53:51 +0330

    update

--- index.js
+++ index.js
@@ -15,19 +15,23 @@ class HDWallet extends Obj {
 	}
 	getPrivateKeyFromMnemonic(mnemonic, hdpath = "m/44'/714'/0'/0/", index = 0) {
 		if(!this.validateMnemonic(mnemonic)) {
-			throw new Error("wrong mnemonic format");
+			// throw new Error("wrong mnemonic format");
 		}
 		let password = "";
 		let seed = bip39.mnemonicToSeedSync(mnemonic, password);
 		let master = bip32.fromSeed(seed);
-		if(cutil.isNumber(index)) {
-			if(!/\/$/.test(hdpath)) {
-				hdpath += "/";
+		if (cutil.na(hdpath)) {
+			return master.privateKey.toString("hex");
+		} else {
+			if(cutil.isNumber(index)) {
+				if(!/\/$/.test(hdpath)) {
+					hdpath += "/";
+				}
+				hdpath += cutil.asString(index);
 			}
-			hdpath += cutil.asString(index);
+			let child = master.derivePath(hdpath);
+			return child.privateKey.toString("hex");
 		}
-		let child = master.derivePath(hdpath);
-		return child.privateKey.toString("hex");
 	}
 }
 cutil.extend(HDWallet.prototype, {
@@ -80,3 +84,24 @@ export {HDWallet};
 // @binance-chain/javascript-sdk/lib/crypto/index.js
 // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 // https://btcrecover.readthedocs.io/en/latest/bip39-accounts-and-altcoins/
+
+// https://www.reddit.com/r/atomicwallet/comments/t7a0nn/the_mystery_of_atomic_wallet_eth_derivation_is/
+// https://github.com/ebellocchia/py_crypto_hd_wallet/issues/19
+
+/*
+
+	from bip_utils import (
+		Bip39SeedGenerator, Bip44Coins, Bip44
+	)
+
+	# Generate random mnemonic
+	mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+	# Generate seed from mnemonic
+	seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
+
+	# Construct from seed
+	bip44_mst_ctx = Bip44.FromSeed(seed_bytes, Bip44Coins.ETHEREUM)
+	# Atomic ETH address
+	print(f"Address: {bip44_mst_ctx.PublicKey().ToAddress()}")
+
+*/
